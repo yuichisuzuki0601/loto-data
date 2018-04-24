919604047e912b91d39515805a18b101987f2330
yuichi <ys.development0601@gmail.com>
2018-04-24 21:24:30 +0900

    結果に曜日を出すように変更

--- src/main/java/jp/co/sbro/util/LotoDataDateFormat.java
+++ src/main/java/jp/co/sbro/util/LotoDataDateFormat.java
@@ -1,13 +1,14 @@
 package jp.co.sbro.util;
 
 import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class LotoDataDateFormat extends SimpleDateFormat {
 
 	private static final long serialVersionUID = 1L;
 
 	public LotoDataDateFormat() {
-		super("yyyy/MM/dd");
+		super("yyyy/MM/dd(E)", Locale.JAPANESE);
 	}
 
 }
